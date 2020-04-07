5dd378bc1ed73c19429f1864b58211f945c7568a
George Yakovlev <yakovlev.ga@live.ru>
2020-04-07 14:12:04 +0300

    React.lazy dropped

--- src/default/App.tsx
+++ src/default/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Route1 from 'frontm/routes/Route1';
+import Route2 from 'frontm/routes/Route2';
+import Route3 from 'frontm/routes/Route3';
+
 import { Provider } from 'frontm/components/ui/Provider'
-const Route1 = React.lazy(() => import('frontm/routes/Route1'));
-const Route2 = React.lazy(() => import('frontm/routes/Route2'));
-const Route3 = React.lazy(() => import('frontm/routes/Route3'));
+// const Route1 = React.lazy(() => import('frontm/routes/Route1'));
+// const Route2 = React.lazy(() => import('frontm/routes/Route2'));
+// const Route3 = React.lazy(() => import('frontm/routes/Route3'));
 
 export const App: React.FC = () => {
   return (
